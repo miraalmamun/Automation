91aacd251b4875e7bdaecdc2d334f463d48663f6
Rainbow <miraalmamun@gmail.com>
2020-06-18 18:35:03 -0400

    Add files via upload
    Third project

---
+++ javaSimpleProject/src/javaSimpleProject/Simple.java
@@ -0,0 +1,22 @@
+package javaSimpleProject;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = x;
+		
+		if((x==y)&&
+				(y==10))
+		{
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
